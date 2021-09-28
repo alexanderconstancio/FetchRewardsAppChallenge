0d2946ab435738475237e5f65cb11405cb333ad1
AC <arcbiz7@gmail.com>
2021-09-27 20:49:57 -0500

    Initial Commit

---
+++ Fetch Rewards Code Challenge Alex CTests/Fetch_Rewards_Code_Challenge_Alex_CTests.swift
@@ -0,0 +1,33 @@
+//
+//  Fetch_Rewards_Code_Challenge_Alex_CTests.swift
+//  Fetch Rewards Code Challenge Alex CTests
+//
+//  Created by Alex Constancio on 9/27/21.
+//
+
+import XCTest
+@testable import Fetch_Rewards_Code_Challenge_Alex_C
+
+class Fetch_Rewards_Code_Challenge_Alex_CTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
